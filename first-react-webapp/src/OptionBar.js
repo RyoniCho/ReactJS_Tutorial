1c53c44a8ed845518548c0dac35792a74ec813c0
조을연 <carpinus0@gmail.com>
2024-08-19 18:41:16 +0900

    Optionbar fix

--- first-react-webapp/src/OptionBar.js
+++ first-react-webapp/src/OptionBar.js
@@ -8,34 +8,36 @@ function OptionBar({ isAuthenticated, actors, selectedActor, setSelectedActor, s
 
     return (
         <div className="option-bar">
-            <select value={selectedCategory} onChange={e => setSelectedCategory(e.target.value)}>
-                <option value="">All</option>
-                {filteredCategories.map((cate,index) => 
-                        (<option key={index} value={cate}>
-                            {cate}
-                        </option>)
-                    )}
-            </select>
-
-            <select value={selectedActor} onChange={e => setSelectedActor(e.target.value)}>
-                <option value="">All Actors</option>
-                {actors.map(actor => (
-                   <option key={actor._id} value={actor.name}>
-                                {actor.name}
-                            </option>
-                ))}
-            </select>
-
-            <select value={sortOrder} onChange={e => setSortOrder(e.target.value)}>
-                <option value="asc">Release Date: Ascending</option>
-                <option value="desc">Release Date: Descending</option>
-            </select>
-
-            <select value={owned} onChange={e => setOwned(e.target.value)}>
-                <option value="all">All</option>
-                <option value="true">Owned</option>
-                <option value="false">Not Owned</option>
-            </select>
+           
+                <select className="option-item" value={selectedCategory} onChange={e => setSelectedCategory(e.target.value)}>
+                    <option value="">All</option>
+                    {filteredCategories.map((cate,index) => 
+                            (<option key={index} value={cate}>
+                                {cate}
+                            </option>)
+                        )}
+                </select>
+            
+                <select className="option-item"  value={selectedActor} onChange={e => setSelectedActor(e.target.value)}>
+                    <option value="">All Actors</option>
+                    {actors.map(actor => (
+                    <option key={actor._id} value={actor.name}>
+                                    {actor.name}
+                                </option>
+                    ))}
+                </select>
+           
+                <select className="option-item" value={sortOrder} onChange={e => setSortOrder(e.target.value)}>
+                    <option value="asc">Release Date: Ascending</option>
+                    <option value="desc">Release Date: Descending</option>
+                </select>
+            
+                <select className="option-item"  value={owned} onChange={e => setOwned(e.target.value)}>
+                    <option value="all">All</option>
+                    <option value="true">Owned</option>
+                    <option value="false">Not Owned</option>
+                </select>
+            
         </div>
     );
 }
