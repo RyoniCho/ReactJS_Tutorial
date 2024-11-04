46b6e8ceab02901e1bb9dfefa030d8418dd30ae7
조을연 <carpinus0@gmail.com>
2024-11-04 21:57:42 +0900

    Loading progress dim 처리

--- first-react-webapp/src/AMovieList.js
+++ first-react-webapp/src/AMovieList.js
@@ -386,7 +386,11 @@ const AMovieList = ({isAuthenticated,isNSFWContentBlured}) => {
                         }
                     </div>
                 ))}
-                {isLoading && <div className="loader"></div>}
+                {isLoading && (
+                    <div className="loader-container">
+                        <div className="loader"></div>
+                    </div>
+                )}
             </div>
         </div>
     </div>
